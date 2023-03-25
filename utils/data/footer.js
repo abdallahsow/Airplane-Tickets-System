37c28cab1332539d961e33ff558b44b49d0848ea
Abdallah Sow <abdallahsow700@gmail.com>
2023-03-25 21:29:17 +0300

    Footer data folder added

---
+++ utils/data/footer.js
@@ -0,0 +1,27 @@
+import Facebook from "public/images/socialMedia/facebook.svg";
+import LinkedIn from "public/images/socialMedia/linkedin.svg";
+import Instagram from "public/images/socialMedia/instagram.svg";
+import Twitter from "public/images/socialMedia/twitter.svg";
+
+export const socialMediaIcons = [
+  {
+    id: 1,
+    src: Facebook,
+    alt: "Facebook",
+  },
+  {
+    id: 2,
+    src: LinkedIn,
+    alt: "LinkedIn",
+  },
+  {
+    id: 3,
+    src: Instagram,
+    alt: "Instagram",
+  },
+  {
+    id: 4,
+    src: Twitter,
+    alt: "Twitter",
+  },
+];
