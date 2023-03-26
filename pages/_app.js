1897c791624e949b4cfa2987be4f2d47d283648d
Abdallah Sow <abdallahsow700@gmail.com>
2023-03-26 14:22:24 +0300

    Fixed the non pure selector errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import Layout from "components/Layout";
+import styles from "styles/globals.module.scss";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <body className={styles.body} />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
