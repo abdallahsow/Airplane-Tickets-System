bbd811290f4ebb473ae4f1efc18388d8112cc0a8
Abdallah Sow <abdallahsow700@gmail.com>
2023-03-26 22:17:58 +0300

    Faq data added

---
+++ utils/data/hero.js
@@ -0,0 +1,64 @@
+export const faqData = [
+  {
+    id: 1,
+    question: "How to reserve plane tickets online",
+    answer: `To make an online flight reservation over the internet, sign in at www.flypgs.com/en or download the Pegasus mobile app on your mobile device.
+
+Then, create a list of available flights by entering your points of departure and arrival, and your travel dates. Select your flight, make any additional selections and enter your information. Then you can reserve your ticket securely.`,
+  },
+  {
+    id: 2,
+    question: "Is it cheaper to book flights at the last minute?",
+    answer: `Contrary to popular belief, booking flights at the last minute is not necessarily cheaper. In fact, it can often be more expensive, especially during peak travel times when demand for flights is high.
+
+Pegasus Airlines use dynamic pricing, which means that the price of a flight can fluctuate based on factors such as demand, time of year, and available seats. When you book a flight at the last minute, you're often competing with other travelers who are also looking to book seats on the same flight. This increased demand can cause prices to rise.
+
+In addition, Pegasus Airlines may sometimes offer discounts for booking in advance, as it helps them to better manage their capacity and plan for the number of passengers on a particular flight.
+
+That being said, it's generally best to book your flight as far in advance as possible to secure the best price.`,
+  },
+  {
+    id: 3,
+    question: "What is the best day of the week to buy plane tickets?",
+    answer: `There is no one "best" day of the week to buy plane tickets that applies universally to all airlines and routes. However, there are some general trends and tips that may be helpful in finding cheaper flights:
+
+Book in advance: Generally, the earlier you book your flight, the more likely you are to get a better price.
+
+Avoid peak travel times: Flights during holidays and weekends tend to be more expensive. Instead, consider traveling during off-peak times, such as midweek.
+
+Be flexible with your travel dates: Sometimes, shifting your travel dates by a day or two can make a significant difference in price.
+
+Set up price alerts: Many travel websites and airlines offer price alert services that will notify you when the price of a particular flight changes.
+
+Consider alternative airports: Sometimes, flying into or out of a smaller airport near your destination can be less expensive.
+
+Ultimately, the best day to buy plane tickets will vary based on the specific route, and timing of your travel plans. It's a good idea to do some research and compare prices across multiple dates to find the best deal for your specific needs.`,
+  },
+  {
+    id: 4,
+    question: "How do I purchase a cheap flight ticket?",
+    answer: `Buying cheap flight tickets with Pegasus Airlines is really simple. Just enter the destination you're travelling to at flypgs.com or on the mobile app to list available flights, then choose the cheapest flight on the date that suits you.`,
+  },
+  {
+    id: 5,
+    question: "How do I find cheap flights?",
+    answer: `If your travel dates are flexible and you want to find the cheapest ticket, follow these steps: Don't leave reservations to the last minute.
+Try to book your reservations as early as possible.
+If you're not sure about your travel dates, fix the price of cheap flight tickets you've found.
+Keep an eye on Pegasus' special offers.
+Keep an eye on seasonal introductory offers.
+Try not to travel at weekends and during public holidays.
+Travel outside of peak season.`,
+  },
+  {
+    id: 6,
+    question:
+      "How long before my trip should I buy my ticket to benefit from cheap prices?",
+    answer: `Flight ticket prices can differ according to daily, weekly and monthly time periods. According to historical data analysis, it can be more advantageous for domestic passengers to make reservations 3 weeks before the flight date and for international passengers 4 weeks before the flight date. In order to easily find cheap flight tickets, it’s important to be flexible in choosing airline companies and travel dates. If there are sudden changes or unexpected flight plans in your business or holiday travels, you can find the right ticket for you with the offers of Ucuzabilet.`,
+  },
+  {
+    id: 7,
+    question: "What is a flight ticket PNR enquiry?",
+    answer: `You can check your ticket with “PNR”, which means “Passenger Name Record” and you can quickly query all flight details by entering name or surname information. The PNR code is sent to you via message or e-mail after purchase. You can perform your transactions with the PNR code at https://www.ucuzabilet.com/pnr-sorgulama.`,
+  },
+];
