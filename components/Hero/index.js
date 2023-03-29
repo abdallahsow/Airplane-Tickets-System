d1948ad897cd0db082c88b90adc4ad8585e209e9
Abdallah Sow <abdallahsow700@gmail.com>
2023-03-29 15:00:56 +0300

    Hero Layout Updated

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,9 +1,11 @@
+import Promotions from "components/Promotions";
 import Faq from "components/Faq";
-import styles from "./Hero.module.scss";
+import styles from "./hero.module.scss";
 
 export default function Hero() {
   return (
     <section className={styles.hero}>
+      <Promotions />
       <Faq />
     </section>
   );
